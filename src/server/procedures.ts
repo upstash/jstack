02ccec0943d8be5433415f721a1218c8edc515dc
Josh <josh@upstash.com>
2024-09-09 12:37:36 +0200

    fix: cloudflare deployment

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,11 +1,13 @@
-import { db } from "@/db"
+import { Pool } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client"
 import { Redis } from "@upstash/redis/cloudflare"
 import { env } from "hono/adapter"
 import { cacheExtension } from "./__internals/db/cache-extension"
 import { j } from "./__internals/j"
 
 /**
- * Middleware for providing a built-in cache with your Prisma database
+ * Middleware for providing a built-in cache with your Prisma database.
  *
  * You can remove this if you don't like it, but caching can massively speed up your database queries.
  */
@@ -13,15 +15,23 @@ import { j } from "./__internals/j"
 const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
   const variables = env(c)
 
+  const pool = new Pool({
+    connectionString: variables.DATABASE_URL,
+  })
+
+  const adapter = new PrismaNeon(pool)
+
   const redis = new Redis({
     token: variables.REDIS_TOKEN,
     url: variables.REDIS_URL,
   })
 
-  const extendedDb = db.$extends(cacheExtension({ redis }))
+  const db = new PrismaClient({
+    adapter,
+  }).$extends(cacheExtension({ redis }))
 
   // Whatever you put inside of `next` is accessible to all following middlewares
-  return await next({ db: extendedDb })
+  return await next({ db })
 })
 
 /**
