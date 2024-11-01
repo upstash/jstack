66f060b34223c4f8a01ee90296d4c819d0c03707
Josh <josh@upstash.com>
2024-11-01 15:07:17 +0100

    fix: base url construction

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -5,9 +5,23 @@ import { StatusCode } from "hono/utils/http-status"
 import superjson from "superjson"
 
 const getBaseUrl = () => {
-  return process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/"
-    : "https://<YOUR_DEPLOYED_WORKER_URL>/"
+  // browser should use relative path
+  if (typeof window !== "undefined") {
+    return ""
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000/"
+  }
+
+  // if deployed to vercel, use vercel url
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  // assume deployment to cloudflare workers otherwise, you'll get this URL after running
+  // `npm run deploy`, which deploys your server to cloudflare
+  return "https://<YOUR_DEPLOYED_WORKER_URL>/"
 }
 
 export const baseClient = hc<AppType>(getBaseUrl(), {
