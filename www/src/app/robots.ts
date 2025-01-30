fe43a2258d80f05bc970a712ccffcc4515ff66b4
Josh <josh@upstash.com>
2025-01-30 09:02:41 +0100

    seo: disallow api crawling

--- www/src/app/robots.ts
+++ www/src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/api",
     },
     sitemap: "https://jstack.app/sitemap.xml",
   }
